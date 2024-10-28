1ec7f33fe8f342e0e566a25f7c03d6dedefa1064
isabelldavis <iad2675@email.vccs.edu>
2024-10-28 18:55:17 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#Name: Isabell Davis
+#Prog Purpose:Hello World
+
+print('Hello World! This is my first Phython Program.')
+print('I can program in HTML, which is awesome.')
+print('However, HTML cannot do arithmetic.')
+print('But Python can: \n\t Add \n\t Multiply \n\t Divide')
